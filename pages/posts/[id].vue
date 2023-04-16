a5ce3030f8a91d02529c2dbb9586081a08035336
Ahmed Zaher <contact@ahmedzaher.net>
2023-04-16 17:38:08 -0600

    await data from sanity

--- pages/posts/[id].vue
+++ pages/posts/[id].vue
@@ -8,7 +8,8 @@
 <script setup lang="ts">
   const { id } = useRoute().params;
   const query = groq`*[_type == "post" && slug.current==$id]{...}`;
-  const { data } = useSanityQuery(query, { id });
+
+  const { data } = await useSanityQuery(query, { id });
   const post = data;
 
   //data later
